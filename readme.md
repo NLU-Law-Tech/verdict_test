07e9630cbe11f10d0fe3929a210f18f086df9982
seanbbear <k7489759@gmail.com>
2020-07-06 02:12:46 +0000

    first commit

---
+++ readme.md
@@ -0,0 +1,24 @@
+input格式如下:
+
+"content_id" 為自定義的判決書id
+"name" 為該篇判決書的被告 (可能有多位被告，故以 content_id + name才可辦識)
+"job_location" 為被告工作 單位
+"job_title" 為被告 職稱
+"laws" 為所犯法條
+
+[
+	{
+		"content_id" : "2" ,
+		"name" : "吳柯森" ,
+		"job_location" : ["停車場"] ,
+		"job_title" : ["管理員"] ,
+		"laws" : ["中華民國刑法第276條第1項","中華民國刑法第140條"]
+    },
+	{
+		"content_id" : "3" ,
+		"name" : "林志清" ,
+		"job_location" : [""] ,
+		"job_title" : ["自由業","管理員"] ,
+		"laws" : ["中華民國刑法第185條之3","中華民國刑法第140條"]
+	}
+]
---
+++ verdict_test.py
@@ -0,0 +1,74 @@
+import json
+import numpy
+
+# ans_list及predict_list為整串預測出的答案list
+# 計算每一篇判決書之分數
+def Score_calculate(ans_list,predict_list):
+    temp_em = 0
+    temp_inter = 0
+    if ans_list == predict_list:
+        temp_em += 1
+    # set(ans_list).intersection(set(predict_list)) # 交集個數
+    temp_inter += len(set(ans_list).intersection(set(predict_list))) / len(ans_list)
+    return temp_em,temp_inter
+
+if __name__ == "__main__":
+    with open('ans.json', 'r', encoding='utf-8') as fp:
+        ans = json.loads(fp.read())
+    with open('predict.json', 'r', encoding='utf-8') as fp:
+        predict = json.loads(fp.read())
+    # 總分 list
+    em_total = []
+    inter_total = []
+    # 單位 職稱與法條的個別分數 list
+    em_loc = []
+    em_tit = []
+    em_law = []
+    inter_loc = []
+    inter_tit = []
+    inter_law = []
+    for ans_defendant in ans:
+        for pred_defendant in predict:
+            if ans_defendant['content_id'] == pred_defendant['content_id']:  
+                # 每篇的分數 
+                em_loc_score = 0
+                em_title_score = 0
+                em_laws_score = 0
+                inter_loc_score = 0
+                inter_title_score = 0
+                inter_laws_score = 0
+                if ans_defendant['name'] == pred_defendant['name']:
+                    em_loc_temp, inter_loc_temp = Score_calculate(ans_defendant['job_location'],pred_defendant['job_location'])
+                    em_tit_temp, inter_tit_temp = Score_calculate(ans_defendant['job_title'],pred_defendant['job_title'])
+                    em_law_temp, inter_law_temp = Score_calculate(ans_defendant['laws'],pred_defendant['laws'])
+                    
+                    em_loc_score += em_loc_temp
+                    em_title_score += em_tit_temp
+                    em_laws_score += em_law_temp
+                    inter_loc_score += inter_loc_temp
+                    inter_title_score += inter_tit_temp
+                    inter_laws_score += inter_law_temp
+                    # 印出每個被告單位 職稱及法條的分數
+                    # print("id:",ans_defendant['content_id'])
+                    # print(em_loc_score, em_title_score, em_laws_score)
+                    # print(inter_loc_score, inter_title_score, inter_laws_score)
+                    
+                    
+
+                    em_loc.append(em_loc_score)
+                    em_tit.append(em_title_score)
+                    em_law.append(em_laws_score)
+                    inter_loc.append(inter_loc_score)
+                    inter_tit.append(inter_title_score)
+                    inter_law.append(inter_laws_score)
+                    
+                    em_total += [em_loc_score,em_title_score,em_laws_score]
+                    inter_total += [inter_loc_score,inter_title_score,inter_laws_score]
+    # 印出個別分數
+    print('em分數: ')
+    print("{:.2f}".format(numpy.mean(em_loc)),"{:.2f}".format(numpy.mean(em_tit)),"{:.2f}".format(numpy.mean(em_law)))
+    print('交集分數: ')
+    print("{:.2f}".format(numpy.mean(inter_loc)),"{:.2f}".format(numpy.mean(inter_tit)),"{:.2f}".format(numpy.mean(inter_law)))
+    # 印出總分
+    print("{:.2f}".format((numpy.mean(em_total))))
+    print("{:.2f}".format(numpy.mean(inter_total)))
