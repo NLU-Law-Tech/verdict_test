3a91ce6c12a01c5734ac36c80dce1effd73e5f4c
wmn <wuemily0812@gmail.com>
2020-07-22 08:37:06 +0800

    add fuzzy match and count up verdict

--- verdict_test.py
+++ verdict_test.py
@@ -1,5 +1,6 @@
 import json
 import numpy
+from fuzzywuzzy import fuzz
 
 # ans_list及predict_list為整串預測出的答案list
 # 計算每一篇判決書之分數
@@ -17,10 +18,16 @@ def exactmatch(ans_list, predict_list):
         temp_em += 1
     return temp_em
 
+def fuzzymatch(ans_list, predict_list):
+    temp_fuzzy = 0
+    if fuzz.ratio(ans_list, predict_list) > 80:
+        temp_fuzzy += 1
+    return temp_fuzzy
+
 def intersect(ans_list, predict_list):
     temp_inter = 0
     if len(ans_list) == 0:
-        temp_inter = 1
+        temp_inter = 0
     else:
         temp_inter += len(set(ans_list).intersection(set(predict_list))) / len(ans_list)
     return temp_inter
@@ -32,10 +39,10 @@ def precision(ans_list, predict_list):
     tp = len(ans & predict)
     fp = len(predict) - tp
     if len(ans_list) == 0:
-        temp_precision = 1
+        temp_precision = 0
     else:
         try: # tp > 0
-            temp_precision += float(tp/(tp+fp))
+            temp_precision = float(tp/(tp+fp))
         except:
             temp_precision = 0
     return temp_precision
@@ -47,10 +54,10 @@ def recall(ans_list, predict_list):
     tp = len(ans & predict)
     fn = len(ans) - tp
     if len(ans_list) == 0:
-        temp_recall = 1
+        temp_recall = 0
     else:
         try: # tp > 0
-            temp_recall += float(tp/(tp+fn))
+            temp_recall = float(tp/(tp+fn))
         except:
             temp_recall = 0
     return temp_recall
@@ -60,10 +67,26 @@ def f1score(ans_list, predict_list):
     prec = precision(ans_list, predict_list)
     rec = recall(ans_list, predict_list)
     try:
-        temp_f1 += 2 * prec * rec/ (prec + rec)
+        temp_f1 = 2 * prec * rec/ (prec + rec)
     except ZeroDivisionError:
         temp_f1 = 0
-    return temp_f1
+    return temp_f1 
+
+# 統計判決書有幾篇幾個被告
+def countup(ans_file = 'ans.json'):
+    with open(ans_file, 'r', encoding = 'utf-8') as fp:
+        ans = json.loads(fp.read())
+
+    verdict_list = []
+    verdict_name = []
+    for content in ans:
+        for key, value in content.items():
+            if key == 'content_id':
+                verdict_list.append(value)
+            elif key == 'name':
+                verdict_name.append(value)
+    print('判決書數量 : ' + str(len(set(verdict_list))))
+    print('被告數量   : ' + str(len(set(verdict_name))))
 
 def score_calculate(ans_list, predict_list):
 
@@ -82,7 +105,10 @@ def score_calculate(ans_list, predict_list):
     temp_f1 = 0
     temp_f1 += f1score(ans_list, predict_list)
 
-    return temp_em, temp_inter, temp_precision, temp_recall, temp_f1
+    temp_fuzzy = 0
+    temp_fuzzy += fuzzymatch(ans_list, predict_list)
+
+    return temp_em, temp_inter, temp_precision, temp_recall, temp_f1, temp_fuzzy
 
 def main(ans_file = 'ans.json', predict_file = 'predict.json'):
 
@@ -92,7 +118,7 @@ def main(ans_file = 'ans.json', predict_file = 'predict.json'):
         predict = json.loads(fp.read())
 
     # 總分 list
-    em_total, inter_total, prec_total, rec_total, f1_total = ([] for i in range(5))
+    em_total, inter_total, prec_total, rec_total, f1_total, fuzzy_total = ([] for _ in range(6))
 
     # 單位 職稱與法條的個別分數 list
     em_loc, em_tit, em_law = ([] for _ in range(3))
@@ -100,6 +126,7 @@ def main(ans_file = 'ans.json', predict_file = 'predict.json'):
     prec_loc, prec_tit, prec_law = ([] for _ in range(3))
     rec_loc, rec_tit, rec_law = ([] for _ in range(3))
     f1_loc, f1_tit, f1_law = ([] for _ in range(3))
+    fuzzy_loc, fuzzy_tit, fuzzy_law = ([] for _ in range(3))
 
     for ans_defendant in ans:
         for pred_defendant in predict:
@@ -111,12 +138,13 @@ def main(ans_file = 'ans.json', predict_file = 'predict.json'):
                 precision_loc_score = precision_title_score = precision_laws_score = 0
                 recall_loc_score = recall_title_score = recall_laws_score = 0
                 f1_loc_score = f1_title_score = f1_laws_score = 0
+                fuzzy_loc_score = fuzzy_title_score = fuzzy_laws_score = 0
 
                 if ans_defendant['name'] == pred_defendant['name']:
 
-                    em_loc_temp, inter_loc_temp, prec_loc_temp, rec_loc_temp, f1_loc_temp = score_calculate(ans_defendant['job_location'], pred_defendant['job_location'])
-                    em_tit_temp, inter_tit_temp, prec_tit_temp, rec_tit_temp, f1_tit_temp = score_calculate(ans_defendant['job_title'], pred_defendant['job_title'])
-                    em_law_temp, inter_law_temp, prec_law_temp, rec_law_temp, f1_law_temp = score_calculate(ans_defendant['laws'], pred_defendant['laws'])
+                    em_loc_temp, inter_loc_temp, prec_loc_temp, rec_loc_temp, f1_loc_temp, fuzzy_loc_temp = score_calculate(ans_defendant['job_location'], pred_defendant['job_location'])
+                    em_tit_temp, inter_tit_temp, prec_tit_temp, rec_tit_temp, f1_tit_temp, fuzzy_tit_temp = score_calculate(ans_defendant['job_title'], pred_defendant['job_title'])
+                    em_law_temp, inter_law_temp, prec_law_temp, rec_law_temp, f1_law_temp, fuzzy_law_temp = score_calculate(ans_defendant['laws'], pred_defendant['laws'])
 
 
                     em_loc_score, em_title_score, em_laws_score = em_loc_temp+em_loc_score, em_tit_temp+em_title_score, em_law_temp+em_laws_score
@@ -124,6 +152,7 @@ def main(ans_file = 'ans.json', predict_file = 'predict.json'):
                     precision_loc_score, precision_title_score, precision_laws_score = prec_loc_temp+precision_loc_score, prec_tit_temp+precision_title_score, prec_law_temp+precision_laws_score
                     recall_loc_score, recall_title_score, recall_laws_score = rec_loc_temp+recall_loc_score, rec_tit_temp+recall_title_score, rec_law_temp+recall_laws_score
                     f1_loc_score, f1_title_score, f1_laws_score = f1_loc_temp+f1_loc_score, f1_tit_temp+f1_title_score, f1_law_temp+f1_laws_score
+                    fuzzy_loc_score, fuzzy_title_score, fuzzy_laws_score = fuzzy_loc_temp+fuzzy_loc_score, fuzzy_tit_temp+fuzzy_title_score, fuzzy_law_temp+fuzzy_laws_score
 
                     # 印出每個被告單位 職稱及法條的分數
                     # print("id:",ans_defendant['content_id'])
@@ -153,33 +182,43 @@ def main(ans_file = 'ans.json', predict_file = 'predict.json'):
                     f1_tit.append(f1_title_score)
                     f1_law.append(f1_laws_score)
 
+                    fuzzy_loc.append(fuzzy_loc_score)
+                    fuzzy_tit.append(fuzzy_title_score)
+                    fuzzy_law.append(fuzzy_laws_score)
+
                     em_total += [em_loc_score, em_title_score, em_laws_score]
                     inter_total += [inter_loc_score, inter_title_score, inter_laws_score]
                     prec_total += [precision_loc_score, precision_title_score, precision_laws_score]
                     rec_total += [recall_loc_score, recall_title_score, recall_laws_score]
                     f1_total += [f1_loc_score, f1_title_score, f1_laws_score]
+                    fuzzy_total += [fuzzy_loc_score, fuzzy_title_score, fuzzy_laws_score]
 
     # 印出個別分數
     print('em分數: ')
     print("單位: " + "{:.2f}".format(numpy.mean(em_loc)),"職稱: " + "{:.2f}".format(numpy.mean(em_tit)),"所犯法條: " + "{:.2f}".format(numpy.mean(em_law)))
-    print('交集分數: ')
-    print("單位: " + "{:.2f}".format(numpy.mean(inter_loc)),"職稱: " + "{:.2f}".format(numpy.mean(inter_tit)),"所犯法條: " + "{:.2f}".format(numpy.mean(inter_law)))
+    print('fuzzymatch: ')
+    print("單位: " + "{:.2f}".format(numpy.mean(fuzzy_loc)),"職稱: " + "{:.2f}".format(numpy.mean(fuzzy_tit)),"所犯法條: " + "{:.2f}".format(numpy.mean(fuzzy_law)))
+    # print('交集分數: ')
+    # print("單位: " + "{:.2f}".format(numpy.mean(inter_loc)),"職稱: " + "{:.2f}".format(numpy.mean(inter_tit)),"所犯法條: " + "{:.2f}".format(numpy.mean(inter_law)))
     print('precision: ')
     print("單位: " + "{:.2f}".format(numpy.mean(prec_loc)),"職稱: " + "{:.2f}".format(numpy.mean(prec_tit)),"所犯法條: " + "{:.2f}".format(numpy.mean(prec_law)))
     print('recall: ')
     print("單位: " + "{:.2f}".format(numpy.mean(rec_loc)),"職稱: " + "{:.2f}".format(numpy.mean(rec_tit)),"所犯法條: " + "{:.2f}".format(numpy.mean(rec_law)))
     print('f1score: ')
     print("單位: " + "{:.2f}".format(numpy.mean(f1_loc)),"職稱: " + "{:.2f}".format(numpy.mean(f1_tit)),"所犯法條: " + "{:.2f}".format(numpy.mean(f1_law)))
-
+    
 
     # 印出總分
-    print('===============')
+    print('=================')
     print('總分')
     print('Exact Match:', "{:.2f}".format(numpy.mean(em_total)))
-    print('Intersect:', "{:.2f}".format(numpy.mean(inter_total)))
-    print('Precision:', "{:.2f}".format(numpy.mean(prec_total)))
-    print('Recall:', "{:.2f}".format(numpy.mean(rec_total)))
-    print('F1 Score:', "{:.2f}".format(numpy.mean(f1_total)))
+    print('Fuzzy Match:', "{:.2f}".format(numpy.mean(fuzzy_total)))
+    # print('Intersect:', "{:.2f}".format(numpy.mean(inter_total)))
+    print('Precision  :', "{:.2f}".format(numpy.mean(prec_total)))
+    print('Recall     :', "{:.2f}".format(numpy.mean(rec_total)))
+    print('F1 Score   :', "{:.2f}".format(numpy.mean(f1_total)))
+    print('=================')
+    countup()
 
 if __name__ == "__main__":
     main()
