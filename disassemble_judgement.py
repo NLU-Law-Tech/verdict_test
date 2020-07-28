b718b94f938e640dc3835142fa7650b2fb3bb9e6
Cai-Pei-Xuan <qwe7539513@gmail.com>
2020-07-28 03:35:54 +0000

    新增將答案拆成一篇篇的檔案的code

--- disassemble_judgement.py
+++ disassemble_judgement.py
@@ -1,16 +1,17 @@
 # -*-coding:UTF-8 -*-
 import json
+import os
 
 # 將判決書拆成一篇篇的檔案
-def disassemble_origin(judgement_path='new2000.txt'):
+def disassemble_origin(judgement_path='new2000.txt', save_path='judgement_data', _id = '_id'):
+    if not os.path.isdir(save_path):
+        os.mkdir(save_path)
+
     with open(judgement_path,'r',encoding='utf-8') as f:
         for doc in f.readlines():
             doc = json.loads(doc)
-            with open('judgement_data/' + doc['_id'] + '.txt','w',encoding='utf-8') as f2:
+            with open(save_path + '/' + doc[_id] + '.txt','w',encoding='utf-8') as f2:
                 f2.write(json.dumps(doc,ensure_ascii=False,indent=None))
-                f2.close()
-
-    f.close()
 
 # 將那7篇測試判決書從原始資料中拆成一篇篇的檔案
 def disassemble_test(judgement_path='new2000.txt'):
@@ -21,10 +22,26 @@ def disassemble_test(judgement_path='new2000.txt'):
             if doc['_id'] in id_list:
                 with open('test_data/' + doc['_id'] + '.txt','w',encoding='utf-8') as f2:
                     f2.write(json.dumps(doc,ensure_ascii=False,indent=None))
-                    f2.close()
 
-    f.close()
+# 將答案拆成一篇篇的檔案
+def disassemble_ans(ans_path='ans.json', save_path='ans_data'):
+    if not os.path.isdir(save_path):
+        os.mkdir(save_path)
+
+    with open(ans_path,'r',encoding='utf-8') as f:
+        all_ans = json.loads(f.read())
+        id_dict = {}
+
+        for ans in all_ans:
+            if ans['content_id'] not in id_dict.keys():
+                id_dict[ans['content_id']] = []
+            id_dict[ans['content_id']].append(ans)
+
+    for content_id in id_dict.keys():
+        with open(save_path + '/' + content_id + '.json','w',encoding='utf-8') as f2:
+            f2.write(json.dumps(id_dict[content_id],ensure_ascii=False,indent=None))
 
 if __name__ == "__main__":
     disassemble_origin()
     disassemble_test()
+    disassemble_ans()
